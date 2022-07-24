4f549cb998fab40bdf3f7190f6f3eb5f2e2ddab3
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-07-24 16:06:50 -0300

    front Start Henry Food 👍🏼

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,4 +3,4 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducer';
 
-export default store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
