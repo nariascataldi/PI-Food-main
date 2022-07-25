e9b753cee22ced3aeb0d5b695309344c2d88a2eb
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-07-25 13:32:38 -0300

    landing Page video

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/LandingPage.css';
+import Fiambre from "./client/src/assets/video/Fiambre.mp4";
+import ReactPlayer from "react-player";
 
-export default function LandingPage(){
+function LandingPage() {
   console.log("Bienvenidos")
   return (
     <div className='LandingPage'>
-      <h1 className="letras">Bienvenidos a mi super página, espere y será atendido</h1>
+    <ReactPlayer url={Fiambre} />
+
+      {/* <h1 className="letras">Food - Proyecto Individual</h1>
       <Link to='/home'>
         <button className='button' id='button'>Ingresar</button>
-      </Link>
+      </Link> */}
     </div>
-  ) 
+  )
 }
+export default LandingPage;
