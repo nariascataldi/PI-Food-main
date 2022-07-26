4aa6d9d90978e9599c7b96e6f156bd315893bf40
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-07-25 22:49:18 -0300

    Landing Page styles

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/LandingPage.css';
-import Fiambre from "./client/src/assets/video/Fiambre.mp4";
-import ReactPlayer from "react-player";
+import Tabla from '../assets/image/wooden-cutting.png'
+
 
 function LandingPage() {
   console.log("Bienvenidos")
   return (
     <div className='LandingPage'>
-    <ReactPlayer url={Fiambre} />
-
-      {/* <h1 className="letras">Food - Proyecto Individual</h1>
-      <Link to='/home'>
-        <button className='button' id='button'>Ingresar</button>
-      </Link> */}
+      <div className="Tabla"> 
+        <div className="home">
+          <Link to='/home'>
+            <button className='button' id='button'>Recetas</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
