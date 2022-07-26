8db6d6ac860436fa54ab94094f64ae845959bf3d
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-07-26 12:40:55 -0300

    Redirect to home whith stack

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-
-import { 
+import React from 'react';
+import {
   BrowserRouter,
   Routes,
-  Route } from "react-router-dom";
-
-import React from 'react';
+  Route
+} from "react-router-dom";
 
 import LandingPage from './components/LandingPage';
+
+import './App.css';
 
 function App() {
   return (
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/LandingPage.css';
-import Tabla from '../assets/image/wooden-cutting.png'
 
 
 function LandingPage() {
   console.log("Bienvenidos")
+  function saludos(){
+    console.log('Aguarde que lo redirecciono. Gracias');
+    console.log('Mis disculpas, estoy en el stack del Event Loop, :)')
+    redirectStack();
+}
+function redirectStack() { 
+  setTimeout(function(){ window.location.href = "http://localhost:3000/home"; }, 10000); }
+  setTimeout(saludos, 3000);
+  
   return (
     <div className='LandingPage'>
-      <div className="Tabla"> 
+      <div className="Tabla">
         <div className="home">
           <Link to='/home'>
             <button className='button' id='button'>Recetas</button>
           </Link>
+
         </div>
       </div>
     </div>
