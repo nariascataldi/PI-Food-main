b47f8417945b96bc70557817aa57870d1c14228e
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2022-08-11 11:04:51 -0300

    port

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ const dietLoader = async function () {
 var boolean = false;
 // Syncing all the models at once.
 conn.sync({ force: boolean }).then(() => {
-  server.listen(process.env.PORT || 3001, () => {
+  server.listen(process.env.PORT || BACKEND_PORT, () => {
     dietLoader()
     if (!boolean) {
       console.log(`It's listening at ${BACKEND_PORT}, recuerda setear el force a true y aumentar las peticiones a 100 de recetas`); // eslint-disable-line no-console
